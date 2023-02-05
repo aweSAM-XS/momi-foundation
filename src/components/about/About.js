526e60011ed40f22beedb30f7b96ae22eb77c7b0
Samuel Kiprop <sam.kiprop23@gmail.com>
2023-02-05 23:35:08 +0300

    Nothing is Impossible

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,7 +9,7 @@ export default function About() {
                     <img alt='about' src={ezron} className='about-img' />
                 </div>
                 <div className='about-text'>
-                    <input type='radio' name='about' id='about' />
+                    <input type='radio' name='about' id='about' checked />
                     <input type='radio' name='mission' id='mission' />
                     <input type='radio' name='vision' id='vision' />
                     <header>
@@ -23,24 +23,36 @@ export default function About() {
                             Vision
                         </label>
                     </header>
-                    <p>
-                        Momi Foundation is a non-profit organization is
-                        dedicated to empowering our community through education
-                        and technology. We believe that access to these
-                        resources is essential for individuals to reach their
-                        full potential.
-                    </p>
-                    <p>
-                        We have a strong track record of making a difference,
-                        through organizing football tournaments, visiting
-                        children's homes, donating computers, and helping pay
-                        school fees for needy students.
-                    </p>
-                    <p>
-                        We are committed to continuing our work to ensure that
-                        everyone in our community has the opportunity to
-                        succeed.
-                    </p>
+                    <div className='cards'>
+                        <div className='card about-card'>
+                            <p>
+                                Momi Foundation is a non-profit organization is dedicated to empowering our community through education and technology. We believe that access to these resources is essential for individuals to reach their full potential.
+                            </p>
+                            <p>
+                                We have a strong track record of making a
+                                difference, through organizing football
+                                tournaments, visiting children's homes, donating
+                                computers, and helping pay school fees for needy
+                                students.
+                            </p>
+                            <p>
+                                We are committed to continuing our work to
+                                ensure that everyone in our community has the
+                                opportunity to succeed.
+                            </p>
+                        </div>
+                        <div className='card mission-card'>
+                            <p>
+                                Our mission is to empower our community through education and technology. We believe that access to these resources is essential for individuals to reach their full potential. 
+                            </p>
+                        </div>
+                        <div className='card vision-card'>
+                            <p>
+                                Our vision is to see a community where everyone has access to education and technology. We believe that access to these resources is essential for individuals to reach their full potential. We are committed to continuing our work to ensure that everyone in our community has the opportunity to succeed. We also believe that access to these resources is essential for individuals to reach their full potential. We are committed to continuing our work to ensure that everyone in our community has the opportunity to succeed.
+                            </p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
